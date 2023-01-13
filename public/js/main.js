b061668ca11e7ec6817e62657e739075aa50f965
unknown <Charlotte Landuyt>
2023-01-13 11:26:35 +0100

    exo4

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+let exo1 = document.getElementsByClassName("redPurple");
+console.log(exo1);
+
+let exo2 = document.querySelectorAll(".redPurple");
+console.log(exo2);
+
+
+let exo3 = document.querySelectorAll("h1.redPurple");
+console.log(exo3);
+
+
+
+let exo4 = document.querySelectorAll("li, p ,span");
+console.log(exo4);
+
+let exo5 = document.querySelectorAll("h1.redPurple, span.redPurple");
+console.log(exo5);
